b8351f60f0cc83047bfe717c9d4cf65e959e830e
nbscarth <nbscarth@hotmail.com>
2024-09-03 13:48:06 -0400

    init built

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,13 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let sampleNumber = metadata.filter((sample) => sample === metadata[0]['id']);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let sampleMetadata = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
 
@@ -56,21 +56,26 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    for (let i = 0; i < names.length; i++){
+      let options = dropdown.append("option");
+      options.text(names[i])
+      options.attr("value", names[i])
+    };
 
     // Get the first sample from the list
-
-
+    let firstSample = names[0];
+    
     // Build charts and metadata panel with the first sample
-
+    buildMetadata(firstSample);
+    buildCharts(firstSample)
   });
 }
 
